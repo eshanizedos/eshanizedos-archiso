a7c2eba439ba9260e2afd4fa22a23b4c2232ee27
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2023-11-14 04:17:59 +0530

    Update profiledef.sh

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -24,6 +24,5 @@ file_permissions=(
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
   ["/etc/grub.d/40_custom"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/"]="0:0:755"
 )
